5af1a088ca595ed6895202530b29ced95eed45e3
Sami Ekblad <sami@vaadin.com>
2026-07-16 14:06:27 +0300

    Upgrade to Vaadin 25.2 and replace deprecated @Theme with @StyleSheet

--- urlshortener-ui/src/main/java/com/svenruppert/urlshortener/ui/vaadin/AppShell.java
+++ urlshortener-ui/src/main/java/com/svenruppert/urlshortener/ui/vaadin/AppShell.java
@@ -3,18 +3,21 @@ package com.svenruppert.urlshortener.ui.vaadin;
 
 
 import com.svenruppert.dependencies.core.logger.HasLogger;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * Use the @PWA annotation to make the application installable on phones, tablets
  * and some desktop browsers.
  */
 @PWA(name = "An OpenSource URL Shortener", shortName = "URL Shortener")
-@Theme("my-theme")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
+@StyleSheet("styles.css")
 @Push
 public class AppShell
     implements AppShellConfigurator, HasLogger {
